443bd897283570fc2861068df1ea658ed908d0c8
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-08 10:19:03 -0500

    Mostrar estado del servidor socket

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -5,8 +5,23 @@ import { Socket } from 'ngx-socket-io';
   providedIn: 'root'
 })
 export class WebsocketService {
+  public socketStatus = false;
 
   constructor(
     private socket: Socket
-  ) { }
+  ) {
+    this.checkStatus();
+  }
+
+  checkStatus(){
+    this.socket.on('connect', () => {
+      console.log('Conectado al servidor');
+      this.socketStatus = true;
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Conectado al servidor');
+      this.socketStatus = false;
+    });
+  }
 }
